01420d095387daab12d470dc756af778923c26db
Admin <Admin@DESKTOP-8QO1DFF>
2023-04-11 13:15:51 +0530

    oku

---
+++ LocaolBoi/src/simple/Hero.java
@@ -0,0 +1,14 @@
+package simple;
+
+public class Hero {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+           System.out.println("sushant");
+           System.out.println("sushant");
+           System.out.println("sushant");
+
+
+	}
+
+}
